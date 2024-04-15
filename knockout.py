edf3d6c1f081373bfc48181b37ec5ac7feb8ea05
cadetbluee <cadetbluee@icloud.com>
2024-04-15 17:46:59 +0900

    ADd

--- knockout.py
+++ knockout.py
@@ -1,60 +1,78 @@
-import pygame
-import sys
+import pygame, sys
+from button import Button
 import random
 
 pygame.init() # pygame 모듈 초기화
 
-img_knockouts = [ 
-    None,
-    pygame.image.load("images/neko4.png"),
-    pygame.image.load("images/neko5.png"),
-    
-]
+display_width = 650 # 가로 사이즈
+display_height = 977
+SCREEN=pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption("Menu")
 
-neko = [[] for _ in range(10)]
+BG=pygame.image.load('assets/main_background.jpg').convert()
+BG=pygame.transform.smoothscale(BG, SCREEN.get_size())
 
-
-
-turn = 0
-map_y = 10
-map_x = 8
-display_width = 912
-display_height = 768
-bg = pygame.image.load("neko_bg.png")
-cursor = pygame.image.load("neko_cursor.png")
-
-for y in range(map_y):
-    for x in range(map_x):
-        neko[y].append(random.choice(range(1,7)))
-
-
-gameDisplay = pygame.display.set_mode((display_width, display_height)) #스크린 초기화
-pygame.display.set_caption("애니팡")  # 타이틀
-clock = pygame.time.Clock() #Clock 오브젝트 초기화
-
-
-def neko_draw():
-    for y in range(map_y):
-        for x in range(map_x):
-            gameDisplay.blit(img_neko[neko[y][x]], (x*72+20, y*72+20))
-
-
-def game(): # 메인 게임 함수
-    
-    tmr = 0 # 시간 관리 변수
-    # 마우스 클래스 부르기
-    
+def get_font(size): 
+    return pygame.font.SysFont("comicsans", size)
+def play():
     while True:
-        tmr += 1 # 매 시간 1초 증가
-        for event in pygame.event.get(): # 윈도운 X 누를 시 나오게끔
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("black")
+
+        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        gameDisplay.blit(bg,(0,0))
-        neko_draw()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+
         pygame.display.update()
-        clock.tick(20)
 
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+     
+        buttom_img=pygame.image.load("assets/play.png")
+        buttom_img = pygame.transform.scale(buttom_img, (200,100))
+        PLAY_BUTTON = Button(buttom_img, pos=(200, 800), 
+                            text_input="PLAY", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
+
+        QUIT_BUTTON = Button(buttom_img, pos=(500, 800), 
+                            text_input="QUIT", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
+
+
+
+        for button in [PLAY_BUTTON,QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
         
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
 
-game()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+        
+main_menu()
